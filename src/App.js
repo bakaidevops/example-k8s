f6608bc1a33bd183d7583ebeafe139787f83588d
frdevelopergen <erkinbaevbakai8@gmail.com>
2023-03-20 17:13:14 +0600

    refactor code

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <div>
       <h1 className='item' style={{fontSize: '20px'}}>APP</h1>
-      <h2 className='item'>Complete DEMO 2 DONE WELL</h2>
+      <h2 className='item'>Complete DEMO 2 DONE WELL DONE</h2>
 
       {(typeof backendData.users === "undefined") ? (
         <p className='item'>Loading...</p>
